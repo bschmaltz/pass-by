6db44d60f031fd0ae72342fbc1fec1f8e7e10013
Ben Schmaltz <benschmaltz@gmail.com>
2015-01-04 00:31:35 -0500

    autocomplete working

--- app/scripts/controllers/form.js
+++ app/scripts/controllers/form.js
@@ -9,11 +9,24 @@
  */
 angular.module('passByApp')
   .controller('FormCtrl', function ($scope, $location) {
+    $scope.originAutocomplete = new google.maps.places.Autocomplete(document.getElementById('origin'));
+    $scope.destinationAutocomplete = new google.maps.places.Autocomplete(document.getElementById('destination'));
+
     $scope.handleFormSubmit = function(){
       var params = {
-        origin: this.start,
-        destination: this.destination
+        oLat: getLat(this.originAutocomplete.getPlace()),
+        oLon: getLon(this.originAutocomplete.getPlace()),
+        dLat: getLat(this.destinationAutocomplete.getPlace()),
+        dLon: getLon(this.destinationAutocomplete.getPlace())
       };
       $location.path('/results').search(params);
     }
   });
+
+function getLat(place){
+  return place.geometry.location.toString().split(", ")[0].slice(1);
+}
+
+function getLon(place){
+  return place.geometry.location.toString().split(", ")[1].slice(0, -1);
+}
