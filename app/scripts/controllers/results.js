0fb1d2e35a9a6f2bbd3cd2d6ce15b8d6b48442b1
Ben Schmaltz <benschmaltz@gmail.com>
2015-01-04 21:30:54 -0500

    route search results done

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /**
  * @ngdoc function
  * @name passByApp.controller:MainCtrl
@@ -23,6 +21,42 @@ angular.module('passByApp')
     directionsService.route(request, function(result, status) {
       if (status == google.maps.DirectionsStatus.OK) {
         directionsDisplay.setDirections(result);
+
+        var placesService = new google.maps.places.PlacesService($scope.resultMap);
+        var totalDistance = result.routes[0].legs[0].distance.value;
+        var path = result.routes[0].overview_path;
+        var radius = 5000;  //todo: more dynamic radius and/or use of bounds
+        var pathIndex = 0;
+        var atEndOfPath = false;
+
+        while(!atEndOfPath){
+          var request = {
+            location: path[pathIndex],
+            radius: radius,
+            types: "amusement_park|aquarium|art_gallery|museum|park|zoo|cafe|food|meal_takeaway|restaurant|lodging"
+          };
+          placesService.nearbySearch(request, function(results, status){
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+              console.log("search results=", results);
+            }
+          });
+
+          if(pathIndex===path.length-1){atEndOfPath = true;}
+          pathIndex = getNextPathIndex(pathIndex, radius, path);
+        }
       }
     });
   });
+
+function getNextPathIndex(cur, dist, path){
+  var res = cur;
+  var addedDistance = 0;
+  while(path[res+1] !== undefined && addedDistance < dist){
+    addedDistance += google.maps.geometry.spherical.computeDistanceBetween(path[res], path[res+1]);
+    res++;
+  }
+  return res;
+}
+//pick best place per search. if it was in last search, get next best.
+//load elements to dom as you get them.
+//if there's nothing or shit results, just don't add anything
