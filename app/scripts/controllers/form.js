55befcaa7e00bb1108d521dcd5534f09bae73229
Ben Schmaltz <benschmaltz@gmail.com>
2015-01-18 15:54:19 -0500

    errors handled on form

--- app/scripts/controllers/form.js
+++ app/scripts/controllers/form.js
@@ -11,12 +11,28 @@ angular.module('passByApp')
   .controller('FormCtrl', function ($scope, $location) {
     $scope.originAutocomplete = new google.maps.places.Autocomplete(document.getElementById('origin'));
     $scope.destinationAutocomplete = new google.maps.places.Autocomplete(document.getElementById('destination'));
+    $scope.originModel = null;
+    $scope.destinationModel = null;
+    $scope.directionsError = false;
+    var directionsService = new google.maps.DirectionsService();
 
     $scope.handleFormSubmit = function(){
-      var params = {
-        o: this.originAutocomplete.getPlace().formatted_address,
-        d: this.destinationAutocomplete.getPlace().formatted_address
+      var params = {};
+      params.o = (this.originAutocomplete.getPlace() !== undefined) ? this.originAutocomplete.getPlace().formatted_address : this.originModel;
+      params.d = (this.destinationAutocomplete.getPlace() !== undefined) ? this.destinationAutocomplete.getPlace().formatted_address: this.destinationModel;
+      
+      var request = {
+        origin: params.o,
+        destination: params.d,
+        travelMode: google.maps.TravelMode.DRIVING
       };
-      $location.path('/results').search(params);
+      directionsService.route(request, function(result, status) {
+        if (status !== google.maps.DirectionsStatus.OK) {
+          $scope.directionsError = true;
+        }else{
+          $location.path('/results').search(params);
+        }
+        $scope.$apply();
+      });
     }
   });
